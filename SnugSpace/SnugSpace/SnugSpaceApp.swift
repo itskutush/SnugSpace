a8588d2f969d94cf67964a928dae3a3d126aa50f
Srijoni <89831977+itskutush@users.noreply.github.com>
2025-07-19 19:50:12 +0530

    Add files via upload

---
+++ SnugSpace/SnugSpace/SnugSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnugSpaceApp.swift
+//  SnugSpace
+//
+//  Created by admin56 on 19/07/25.
+//
+
+import SwiftUI
+
+@main
+struct SnugSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
